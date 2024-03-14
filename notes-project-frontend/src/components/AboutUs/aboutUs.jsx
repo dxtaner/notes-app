3efdd6055679f7d0bbdb6dcd920925952b00b180
taner <tanerozer16@gmail.com>
2024-03-14 14:20:49 +0300

    aboutus page added

---
+++ notes-project-frontend/src/components/AboutUs/aboutUs.jsx
@@ -0,0 +1,22 @@
+// aboutUs.jsx
+
+import "./aboutUs.css";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>Not Oluşturma Uygulaması</h1>
+      <p>
+        Bu uygulama, React ve React Router kullanılarak oluşturulmuş bir örnek
+        uygulamadır. Bu uygulama, notlarınızı hızlı bir şekilde oluşturmanıza ve
+        düzenlemenize olanak tanır. Her notun başlığını ve içeriğini
+        özelleştirebilirsiniz. Ayrıca, notlarınız düzenli bir şekilde
+        yönetebilirsiniz. Projemi incelemek, katkıda bulunmak veya geri
+        bildirimde bulunmak isterseniz, lütfen GitHub ve LinkedIn profillerimi
+        ziyaret edin. Ayrıca, Mediumdaki yazılarıma göz atabilirsiniz.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
