ce8ef23d038424964034250b3a73fcb0a74b2f2b
taner <tanerozer16@gmail.com>
2024-03-14 14:27:49 +0300

    main jsx added

---
+++ notes-project-frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import "sweetalert2/dist/sweetalert2.min.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
